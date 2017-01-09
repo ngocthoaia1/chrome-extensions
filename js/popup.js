0f2fdd56af1b7401327f69017b425ba411ccda29
Ngoc Thoai Nguyen <ngocthoaia1@gmail.com>
2017-01-09 13:02:52 +0700

    Hide element (#1)
    * hide element
    
    * add hide field

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,9 @@
-// Saves options to chrome.storage
 function save_options() {
   var isHideIframe = document.getElementById('hide-iframe').checked;
+  var isHideElements = document.getElementById('hide-element-checkbox').checked;
   chrome.storage.sync.set({
-    isHideIframe: isHideIframe
+    isHideIframe: isHideIframe,
+    isHideElements: isHideElements
   }, function() {
     // Update status to let user know options were saved.
   });
@@ -13,11 +14,23 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    isHideIframe: true
+    isHideIframe: true,
+    isHideElements: true,
+    hideElements: true
   }, function(items) {
     document.getElementById('hide-iframe').checked = items.isHideIframe;
+    document.getElementById('hide-element-checkbox').checked = items.isHideElements;
   });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('hide-iframe').addEventListener('click',
     save_options);
+
+document.getElementById('hide-element-checkbox').addEventListener('click',
+  save_options);
+
+$('#hide-element-settings').click(function(e) {
+  e.preventDefault();
+  chrome.tabs.create({url: 'chrome-extension://' + chrome.runtime.id + '/html/options.html'});
+})
